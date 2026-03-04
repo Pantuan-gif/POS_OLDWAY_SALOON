a9de1b55fa79a6d1086a0b7610ffd6c6718a1040
Marisuuuu <matthew.larrobis20@gmail.com>
2026-03-04 15:11:10 +0800

    addition of Product Models and UserModels

--- MVVM/MODELS/UserModel.cs
+++ MVVM/MODELS/UserModel.cs
@@ -6,9 +6,25 @@ using System.Threading.Tasks;
 
 namespace POS_OLDWAY_SALOON.MVVM.MODELS
 {
-    public class Posmodel
+    public class UserModel
     {
-        public string username { get; set; }
-        public string password { get; set; }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public int PhoneNumber { get; set; }
+        public string Role { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string ProfileImage { get; set; }
+
+
     }
 }
